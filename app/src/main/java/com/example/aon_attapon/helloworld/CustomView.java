83780e9435a83f226fd0570ee0d6a60e45717d1f
Aon1840 <aonattapon1840.ap@gmial.com>
2018-06-05 23:32:03 +0700

    Input Handle

--- app/src/main/java/com/example/aon_attapon/helloworld/CustomView.java
+++ app/src/main/java/com/example/aon_attapon/helloworld/CustomView.java
@@ -6,11 +6,16 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class CustomView extends View {
 
     private boolean isBlue = false;
+    private boolean isDown = false;
+    private GestureDetector gestureDetector;
 
     public CustomView(Context context) {
         super(context);
@@ -36,6 +41,43 @@ public class CustomView extends View {
     }
 
     private void init() {
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                //Decide: if care event must be continued below method
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+                //Do when case onDown (onDown return to)
+                getParent().requestDisallowInterceptTouchEvent(true);
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                //Action Up
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                isBlue = !isBlue;
+                invalidate();
+                return true;
+
+            }
+        });
     }
 
     private void initWithAttrs(AttributeSet attres, int defStyleAttr, int defStyleRes) {
@@ -59,7 +101,43 @@ public class CustomView extends View {
             p.setColor(0xFFFF0000);
 
         canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+
+        //convert dp to px
+        if (isDown) {
+            p.setColor(0xFF00FF00);
+
+            float px = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    5,
+                    getContext().getResources().getDisplayMetrics());
+            p.setStrokeWidth(px);
+
+            //draw line
+            canvas.drawLine(0,getMeasuredHeight(),getMeasuredWidth(),0,p);
+        }
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //Pass event to GestureDetector
+        gestureDetector.onTouchEvent(event);
 
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                invalidate(); //clear canvas and draw agian
+                isDown = true;
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                isDown = false;
+                invalidate(); //clear canvas and draw agian
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+                isDown = false;
+                invalidate(); //clear canvas and draw agian
+                return true;
+        }
+        return super.onTouchEvent(event);
+    }
 }
