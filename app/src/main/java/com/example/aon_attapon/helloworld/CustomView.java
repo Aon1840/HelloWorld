c514c835f24b428b83bc61fe5d1373fdcbec1b76
Aon1840 <aonattapon1840.ap@gmial.com>
2018-06-05 22:12:07 +0700

    CustomView

--- app/src/main/java/com/example/aon_attapon/helloworld/CustomView.java
+++ app/src/main/java/com/example/aon_attapon/helloworld/CustomView.java
@@ -1,4 +1,43 @@
 package com.example.aon_attapon.helloworld;
 
-public class CustomView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomView extends View{
+
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint p = new Paint();
+        p.setColor(0xFFFF0000);
+        canvas.drawLine(0,0,getMeasuredWidth(),getMeasuredHeight(),p);
+    }
 }
